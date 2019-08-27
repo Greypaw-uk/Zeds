64e33255487b5d6ed4ee6be24b8b484e98838506
Ratstool <36894563+Ratstool@users.noreply.github.com>
2019-08-27 15:35:04 +0100

    Initial commit

---
+++ Graphics.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Net.Mime;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Zeds
+{
+    public static class Graphics
+    {
+        public static Texture2D DrawBackground(Texture2D background)
+        {
+
+            return background;
+        }
+    }
+}
---
+++ zedController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Zeds
+{
+    public static class ZedController
+    {
+        private static Vector2 zone1;
+        private static Vector2 zone2;
+        private static Vector2 zone3;
+        private static Vector2 zone4;
+
+        public static void ZedSpawnLocations()
+        {
+            zone1.X = 0;
+            zone1.Y = 0;
+
+            zone2.X = 800;
+            zone2.Y = 0;
+
+            zone3.X = 0;
+            zone3.Y = 600;
+
+            zone4.X = 800;
+            zone4.Y = 600;
+        }
+    }
+}
