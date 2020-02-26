db92d86d5c81bf7eda34ab9ed6d632dc3a63b889
Ratstool <36894563+Ratstool@users.noreply.github.com>
2020-02-26 16:55:49 +0000

    Pushing through files which were ignored for some unknown reason

---
+++ Engine/Debug/DamageEntities.cs
@@ -0,0 +1,24 @@
+﻿namespace Zeds.Engine.Debug
+{
+    class DamageEntities
+    {
+        public static void DamageHumanPawns()
+        {
+            foreach(var person in EntityLists.HumanList)
+                person.CurrentHealth -= 0.1f;
+        }
+
+        public static void DamageZedPawns()
+        {
+            foreach (var zed in EntityLists.ZedList)
+                zed.CurrentHealth -= 0.1f;
+        }
+
+        public static void DamageBuildings()
+        {
+            foreach (var building in EntityLists.BuildingList)
+                if (building.IsStillStanding)
+                    building.CurrentHealth -= 0.1f;
+        }
+    }
+}
